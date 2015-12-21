20ceb85684764472b013b42da65b18c6063cc81f
jamesrco <james.r.collins@aya.yale.edu>
2015-12-21 16:07:47 -0300

    Initial commit zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname)
+{
+  suppressPackageStartupMessages(require(xcms))
+  suppressPackageStartupMessages(require(CAMERA))
+  suppressPackageStartupMessages(require(LOBSTAHS))
+  load(file.path(find.package("PtH2O2lipids"), "data", "PtH2O2lipids.RData"))
+  assign("PtH2O2lipids", PtH2O2lipids, env = getNamespace("PtH2O2lipids"))
+}
