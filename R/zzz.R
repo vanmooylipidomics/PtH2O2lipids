1a6dc38d22a7b3a11930b58574022b3f5e436d4b
jamesrco <james.r.collins@aya.yale.edu>
2015-12-22 08:52:56 -0300

    Update DESCRIPTION, NAMESPACE, zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,8 @@
   suppressPackageStartupMessages(require(xcms))
   suppressPackageStartupMessages(require(CAMERA))
   suppressPackageStartupMessages(require(LOBSTAHS))
+  suppressPackageStartupMessages(require(methods))
+  suppressPackageStartupMessages(require(utils))
   load(file.path(find.package("PtH2O2lipids"), "data", "PtH2O2lipids.RData"))
   assign("PtH2O2lipids", PtH2O2lipids, env = getNamespace("PtH2O2lipids"))
 }
